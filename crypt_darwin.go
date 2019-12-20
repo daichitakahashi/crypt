c359ed57a6df8e4d5678fabaab38c068da94f198
daichitakahashi <daichitakahashi@DIQ-002.local>
2019-12-20 17:58:34 +0900

    add darwin support

--- crypt_darwin.go
+++ crypt_darwin.go
@@ -4,18 +4,16 @@ import (
 	"unsafe"
 )
 
-// #cgo LDFLAGS: -lcrypt
-// #define _GNU_SOURCE
-// #include <crypt.h>
+// #include <unistd.h>
 // #include <stdlib.h>
 import "C"
 
 // Crypt is wrapper for crypt(3)
 func Crypt(key, salt string) string {
-	data := C.struct_crypt_data{}
+	//data := C.struct_crypt_data{}
 	cKey := C.CString(key)
 	cSalt := C.CString(salt)
-	out := C.GoString(C.crypt_r(cKey, cSalt, &data))
+	out := C.GoString(C.crypt(cKey, cSalt)) //, &data))
 	C.free(unsafe.Pointer(cKey))
 	C.free(unsafe.Pointer(cSalt))
 	return out
